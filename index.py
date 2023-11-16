d24c1e14febc52106b9fc536b288e49591de7b8d
Ivy930626 <chiivy0626@gmail.com>
2023-11-16 21:28:26 +0800

    make it better

--- index.py
+++ index.py
@@ -6,11 +6,16 @@ app = Flask(__name__)
 
 def index():
 	X = "<h1>資管二A 411147013 謝棨亘</h1>"
+	X += "<a href=/myself>關於我自己</a><br>"
 	X += "<a href=/interest>興趣何倫碼測驗結果</a><br>"
 	X += "<a href=/work>最有興趣的MIS相關工作</a><br>"
 	X += "<a href=/about>未來規劃</a><br>"
 	return X
 
+@app.route("/myself")
+def myself():
+	return render_template("myself.html")
+	
 @app.route("/interest")
 def interest():
 	return render_template("interest.html")
